2835b4edb3b65f6f9c2891b5a3e82eb6fe18e6ab
taoyeh <50255085+taoyeh@users.noreply.github.com>
2019-09-20 17:58:28 +0800

    =  =

--- src/components/bottombar.vue
+++ src/components/bottombar.vue
@@ -38,16 +38,19 @@
         </div>
         <div
           class="mytxt"
-        >浙江，简称“浙”，是中华人民共和国省级行政区。省会杭州，位于中国东南沿海，浙江界于东经118°01'-123°10'，北纬27°02'-31°11'之间，东临东海，南接福建，西与安徽、江西相连，北与上海、江苏接壤，境内最大的河流钱塘江，因江流曲折，称之江，又称浙江，省以江名，简称"浙"。浙江省总面积10.55万平方千米。</div>
+        >  <p>浙江，简称“浙”，是中华人民共和国省级行政区。省会杭州，位于中国东南沿海，东临东海，南接福建，西与安徽、江西相连，北与上海、江苏接壤，境内最大的河流钱塘江，因江流曲折，称之江，又称浙江，省以江名，简称"浙"。浙江省总面积10.55万平方千米。浙江地势由西南向东北倾斜，地形复杂。山脉自西南向东北成大致平行的三支。</p> 
+        
+         <p class="mypadding" >乡土文化无论是物质的、非物质的都是不可替代的无价之宝！对乡土文化的保护和延承也必须覆盖物质的、非物质的各个领域，而且保护始终是第一位的。</p>
+         </div>
         <div class="mybottom">
-          <div>杭州 | 宁波 | 温州 | 绍兴 |湖州 |嘉兴| 金华 | 衢州 |台州 |丽水 |舟山</div>
-          <div>由五位队员制作和完成</div>
+          <div>杭州 | 宁波 | 温州 | 绍兴 | 湖州 | 嘉兴 | 金华 | 衢州 |台州 | 丽水 | 舟山</div>
+          <div>浙里文化</div>
         </div>
       </div>
     </div>
     <div class="othermybottom" v-if=" !options.isHome">
-      <div class="myborder">杭州 | 宁波 | 温州 | 绍兴 |湖州 |嘉兴| 金华 | 衢州 |台州 |丽水 |舟山</div>
-      <div>由五位队员制作和完成</div>
+      <div class="myborder">杭州 | 宁波 | 温州 | 绍兴 | 湖州 | 嘉兴 | 金华 | 衢州 |台州 | 丽水 | 舟山</div>
+      <div>浙里文化</div>
     </div>
   </div>
 </template>
@@ -91,7 +94,7 @@ export default {
   }
 }
 .bigcontent {
-  height: 530px;
+  height: 430px;
   width: 100%;
   background-image: url("../assets/images/BG7.jpg");
   display: flex;
@@ -99,7 +102,7 @@ export default {
   align-items: center; //子元素垂直居中
   .content {
     width: 1100px;
-    height: 400px;
+    height: 300px;
     div {
       display: flex;
       flex-direction: row;
@@ -115,19 +118,26 @@ export default {
       fill: var(--main-color);
     }
     .mytxt {
-      margin-bottom: 50px;
-      margin-top: 50px;
+      margin-bottom: 30px;
+      margin-top: 30px;
       line-height: 30px;
-      width: 600px;
+      width: 100%;
       padding-left: 5px;
+      text-indent: 2em;
+      display: grid;
+      grid-template-columns: 6fr  4fr;
+      .mypadding
+      {
+        padding-left: 70px;
+      }
     }
     .mybottom {
       border-top: 2px solid var(--main-color);
-      padding-top: 50px;
+      padding-top: 30px;
       display: flex;
       flex-direction: column;
       div {
-        width: 100%;
+        // width: 100%;
         display: flex;
         flex-direction: row;
         margin-bottom: 5px;
