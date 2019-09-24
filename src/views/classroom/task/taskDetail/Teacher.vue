301d2c542e1e7efcb34985a7cef8a0bcf77fccec
ahbicj <8109887322qq.com>
2019-09-24 21:23:22 +0800

    cursor: pointer;

--- src/views/classroom/task/taskDetail/Teacher.vue
+++ src/views/classroom/task/taskDetail/Teacher.vue
@@ -435,6 +435,7 @@ export default {
         .meansBox_out {
           margin-right: -12px;
           .meansBox {
+            cursor: pointer;
             display: inline-block;
             position: relative;
             margin-bottom: 12px;
